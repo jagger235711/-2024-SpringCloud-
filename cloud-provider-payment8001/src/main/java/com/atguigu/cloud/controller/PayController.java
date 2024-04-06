6030aac064f1a3b00fd4b20cbcf556c72c565eb7
巫文杰 <252587809@qq.com>
2024-04-06 21:35:22 +0800

    consul服务注册与发现组件添加完成 根据以上Git diff信息，可以生成以下commit message：
    1. 在`Main8001.java`文件中，创建一个新的Java类`Main8001`，用于启动Spring Boot应用。
    2. 在`application.yml`文件中，配置了项目端口号8001。
    3. 在`PayController.java`文件中，修改了支付控制器的接口返回值，使用`ResultData`包装返回结果。
    4. 在`PayServiceImpl.java`文件中，添加了`@Service`注解，将`PayServiceImpl`类声明为一个Spring Bean。
    5. 在`pom.xml`文件中，添加了项目依赖。
    
    因此，commit message可以是：
    
    启动类、配置文件、控制器、服务及依赖初始化。

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -58,5 +59,13 @@ public class PayController {
     @Operation(summary = "查询所有流水",description = "查询所有支付流水方法")
 	public ResultData<List<Pay>> getAll() {
 		return ResultData.success(payService.getAll());
+	}
+
+	@Value("${server.port}")
+	private String port;
+
+	@GetMapping(value = "/pay/get/info")
+	public String getInfoByConsul(@Value("${atguigu.info}") String atguiguInfo) {
+		return "atguiguInfo: " + atguiguInfo + "\t port: " + port;
 	}
 }
