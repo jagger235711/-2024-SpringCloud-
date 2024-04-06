f1f32ea18d67c9590775f4b14d5723b1e240fcfd
巫文杰 <252587809@qq.com>
2024-04-06 11:05:50 +0800

    根据以上Git diff信息，可以生成以下commit message：
    1. 在`Main8001.java`文件中，创建一个新的Java类`Main8001`，用于启动Spring Boot应用。
    2. 在`PayController.java`文件中，修改了支付控制器的接口返回值，使用`ResultData`包装返回结果。
    3. 在`PayServiceImpl.java`文件中，添加了`@Service`注解，将`PayServiceImpl`类声明为一个Spring Bean。
    4. 在`pom.xml`文件中，添加了项目依赖。
    
    因此，commit message可以是：
    
    启动类、控制器、服务及依赖初始化。

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -12,8 +12,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderController {
-	public static final String PaymentSrv_URL = "http://localhost:8001";
-
+	//	public static final String PaymentSrv_URL = "http://localhost:8001";
+	public static final String PaymentSrv_URL = "http://cloud-payment-service";//服务注册中心上的微服务名称
 	@Resource
 	private RestTemplate restTemplate;
 
