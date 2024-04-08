c02d7b4fe9ce491eaf51ec7a82e052a75ffd9ef6
巫文杰 <252587809@qq.com>
2024-04-08 15:14:19 +0800

    负载均衡学习结束 根据以上Git diff信息，可以生成以下commit message：
    初始化项目结构及基本配置，包括启动类、配置文件、枚举类、DTO类、配置类、依赖及控制器。

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -5,10 +5,14 @@ import com.atguigu.cloud.resp.ResultData;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -38,5 +42,29 @@ public class OrderController {
 		return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/" + id, ResultData.class, id);
 	}
 
+	@GetMapping(value = "/consumer/pay/get/info")
+	private String getInfoByConsul() {
+		return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+	}
+
+	@Resource
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/consumer/discovery")
+	public String discovery() {
+		List<String> services = discoveryClient.getServices();
+		for (String element : services) {
+			System.out.println(element);
+		}
+
+		System.out.println("===================================");
+
+		List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+		for (ServiceInstance element : instances) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+		}
+
+		return instances.get(0).getServiceId() + ":" + instances.get(0).getPort();
+	}
 
 }
