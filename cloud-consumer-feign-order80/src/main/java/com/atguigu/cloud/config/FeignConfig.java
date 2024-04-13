32c0b57a13fbf858da61b4d46b4dabc3a6985310
巫文杰 <252587809@qq.com>
2024-04-13 21:07:15 +0800

    openFeign日志打印

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +13,10 @@ public class FeignConfig {
 
 		//最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
 //		return new Retryer.Default(100, 1, 3);
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 }
